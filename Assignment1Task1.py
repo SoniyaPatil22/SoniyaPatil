143dc7199949498942b3f0053cad7d228823b49e
SoniyaPatil22 <sonia.dhole9622@gmail.com>
2025-04-16 23:12:25 +0530

    Add files via upload

---
+++ Assignment1Task1.py
@@ -0,0 +1,14 @@
+num1 = input('Enter the first number: ')
+num2 = input('Enter the Second number: ')
+num1 = int(num1)
+num2 = int(num2)
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print('Addition: ', add)
+print('Subtraction: ', sub)
+print('Multiplication: ', mul)
+print('Division: ', div)
