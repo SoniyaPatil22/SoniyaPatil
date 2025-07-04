fc9b841a522e5c30e6adb682a10bd6741671aed3
SoniyaPatil22 <sonia.dhole9622@gmail.com>
2025-07-04 16:45:25 +0530

    Create Assignment2Task1.py

---
+++ Assignment2Task1.py
@@ -0,0 +1,8 @@
+# Step 1: Take integer input from the user
+number = int(input("Enter an number: "))
+
+# Step 2 and 3: Check if the number is even or odd using if-else and display result
+if number % 2 == 0:
+    print(f"The number {number} is even.")
+else:
+    print(f"The number {number} is odd.")
